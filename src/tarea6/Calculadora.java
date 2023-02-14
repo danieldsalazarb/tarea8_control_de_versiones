7a4832da0ecb692afc2ac692d0f3151170babb36
Daniel Salazar <danieldsalazarb@gmail.com>
2023-02-14 09:11:07 +0100

    Se añade método para el calculo de la potencia al cuadrado de un número entero o decimal

--- src/tarea6/Calculadora.java
+++ src/tarea6/Calculadora.java
@@ -40,6 +40,12 @@ public class Calculadora {
         if (num2==0) throw new ArithmeticException("NO PUEDES DIVIDIR POR CERO, MELON");
         return num1/num2;
     }
+/** Se añade método para el calculo de la potencia al cuadrado de un número entero o con decimales
+ * @return regresa el cuadrado de un número entero o decimal
+ */
+    public double cuadrado (double num1){
+        return Math.pow(num1, 2);
+    }
 /** Declaración de variables y sus tipos
  * @param valor1 primer número entero
  * @param valor2 segundo número entero
